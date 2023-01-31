ecccc2d9a9b39b74016c048d20f86c36989a3c84
Laurence Joseph Pakenham-Smith <lpakenham-smith@kevel.co>
2023-01-31 21:56:47 +0000

    Update

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,37 @@
+{-# LANGUAGE InstanceSigs #-}
 module Main where
+import Data.List
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = print myProgram
+
+newtype Program = Program [Rule]
+data Rule = Rule Relation [Relation]
+data Relation = Relation Name [Variable]
+type Name = String
+data Variable = Symbol String | Value String
+
+fact :: Relation -> Rule
+fact r = Rule r []
+
+myProgram :: Program
+myProgram = Program [ fact (Relation "station" [Value "york"])
+                    , Rule (Relation "reachable" [Symbol "x", Symbol "x"]) [(Relation "station" [Symbol "x"])]]
+
+instance Show Variable where
+  show :: Variable -> String
+  show (Symbol s) = s
+  show (Value s) = "\"" ++ s ++ "\""
+
+instance Show Relation where
+  show :: Relation -> String
+  show (Relation name vars) = name ++ "(" ++ intercalate ", " (map show vars) ++ ")"
+
+instance Show Rule where
+  show :: Rule -> String
+  show (Rule head []) = show head ++ "."
+  show (Rule head body) = show head ++ " <-\n  " ++ intercalate ", " (map show body) ++ "."
+
+instance Show Program where
+  show :: Program -> String
+  show (Program rules) = intercalate "\n" (map show rules)
