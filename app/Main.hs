a608f9d3725a88c1b2cf7d5e9c4cddb41babf065
Laurence Joseph Pakenham-Smith <lpakenham-smith@kevel.co>
2023-02-01 13:35:50 +0000

    Partially finished naive evaluator

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@
 module Main where
 import Data.List (intercalate)
 import Text.ParserCombinators.Parsec
+import Data.Set (Set, empty, unions, fromList, union, filter, singleton)
 
 newtype Program = Program [Rule]
 data Rule = Rule Relation [Relation]
@@ -93,7 +94,60 @@ instance Show Program where
   show (Program rules) = intercalate "\n" (map show rules)
 
 parseRunic :: String -> Either ParseError Program
-parseRunic input = parse program "(unknown)" input
+parseRunic = parse program "(unknown)"
+
+data Fact = Fact String [String] deriving (Show, Eq, Ord)
+
+runRunic :: Program -> Set Fact
+runRunic p = runRunic' p empty
+
+
+runRunic' :: Program -> Set Fact -> Set Fact
+runRunic' p facts =
+  let facts' = runRunicStep p facts in
+        if facts == facts'
+        then facts'
+        else runRunic' p facts'
+
+runRunicStep :: Program -> Set Fact -> Set Fact
+runRunicStep (Program rules) facts = unions (map (runRunicRule facts) rules)
+
+-- using facts we know and an existing rule, derive new facts
+runRunicRule :: Set Fact -> Rule -> Set Fact
+runRunicRule facts (Rule (Relation name vars) []) =
+  -- if we are out of relations in the body, if all the vars have unified, create a new fact
+  let rule = Rule (Relation name vars) [] in
+        if ruleSatisfied rule
+        then facts `union` singleton (toFact rule)
+        else facts
+runRunicRule facts (Rule (Relation name vars) rules) = _
+  -- go through each fact we have and try to apply it to the relation at the head of the rule
+
+toFact :: Rule -> Fact
+toFact (Rule (Relation name vars) _) = Fact name (map valueOf vars)
+
+valueOf :: Variable -> String
+valueOf (Value s) = s
+valueOf _ = "NOT FOUND"
+
+-- a rule is satisfied if no symbols remain in the head relation
+ruleSatisfied :: Rule -> Bool
+ruleSatisfied (Rule (Relation _ vars) _) = all isValue vars
+
+isValue :: Variable -> Bool
+isValue (Value _) = True
+isValue _ = False
+
+factsWithName :: String -> Set Fact -> Set Fact
+factsWithName name = Data.Set.filter (factHasName name)
+
+factHasName :: String -> Fact -> Bool
+factHasName n (Fact name _) = n == name
+
+-- TODO: this is where the magic happens. need to unify/substitute in values to find facts that work!
 
 main :: IO ()
-main = print (parseRunic myProgramText)
+main = let parsed = parseRunic myProgramText in
+  case parsed of
+    (Left err) -> print err
+    (Right program) -> print (runRunic program)
